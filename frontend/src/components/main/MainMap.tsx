d702581a12c10653f807cbbb6fbd4fb7fde797aa
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-03-26 16:02:53 +0900

    feat(frontend): add hooks for geolocation

--- frontend/src/components/main/MainMap.tsx
+++ frontend/src/components/main/MainMap.tsx
@@ -1,17 +1,26 @@
+import { useGeoLocation } from "@/hooks/useGeoLocation";
 import {
   Map as KakaoMap,
   MapMarker,
   useKakaoLoader,
 } from "react-kakao-maps-sdk";
 
+const geolocationOptions = {
+  enableHighAccuracy: true,
+  timeout: 1000 * 10,
+  maximumAge: 1000 * 3600 * 24,
+}
+
 export const MainMap = () => {
   useKakaoLoader({
     appkey: import.meta.env.VITE_KAKAOMAP_KEY,
   });
 
+  const { location } = useGeoLocation(geolocationOptions)
+  
   return (
     <KakaoMap
-      center={{ lat: 33.450701, lng: 126.570667 }}
+    center={{ lat: location?.latitude || 33.450701, lng: location?.longitude || 126.570667 }}
       style={{ width: "1000px", height: "600px" }}
       level={3}
     >
